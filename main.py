22f54e6b5c90bc8793109155f5e277cd25708eb9
Mizuki0614 <waon0706@gmail.com>
2019-05-24 23:45:54 +0900

    Add main.py
    main.py::Not completed

---
+++ main.py
@@ -0,0 +1,15 @@
+import test02
+
+#!!!yet!!!
+def main():
+
+    first_page = "https://omairi.club/spots/ranking"
+
+    for i in test02.get_urls(first_page):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
+
+
